5b3a362a29b9792b1e341dbabd10ea19c8a346c3
kayode <bubykay@gmail.com>
2025-01-06 12:15:54 +0100

    fix media query

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,3 +1,11 @@
 document.querySelector('#menu').addEventListener('click', () => {
     document.querySelector('nav').classList.toggle('toggleNav')
 })
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 769) {
+        document.querySelector('nav').classList.remove('toggleNav')
+    } else {
+        document.querySelector('nav').classList.add('toggleNav')
+    }
+})
