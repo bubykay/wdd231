524e24fa7e5f7606ee4198108643da7fc76b8ed2
kayode <bubykay@gmail.com>
2025-01-06 22:42:20 +0100

    fixed course list display on mobile

--- scripts/courseList.js
+++ scripts/courseList.js
@@ -8,7 +8,7 @@ const courses = [
         description:
             'This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.',
         technology: ['Python'],
-        completed: false,
+        completed: true,
     },
     {
         subject: 'WDD',
@@ -19,7 +19,7 @@ const courses = [
         description:
             'This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming. It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.',
         technology: ['HTML', 'CSS'],
-        completed: false,
+        completed: true,
     },
     {
         subject: 'CSE',
@@ -30,7 +30,7 @@ const courses = [
         description:
             'CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call , debug, and test their own functions; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.',
         technology: ['Python'],
-        completed: false,
+        completed: true,
     },
     {
         subject: 'CSE',
@@ -52,7 +52,7 @@ const courses = [
         description:
             'This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.',
         technology: ['HTML', 'CSS', 'JavaScript'],
-        completed: false,
+        completed: true,
     },
     {
         subject: 'WDD',
@@ -63,7 +63,7 @@ const courses = [
         description:
             'This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.',
         technology: ['HTML', 'CSS', 'JavaScript'],
-        completed: true,
+        completed: false,
     },
 ]
 const courseListItemsContainer = document.querySelector('.course-list-items')
